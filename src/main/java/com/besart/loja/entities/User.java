10b1ee1f200a76f8d2ae31de65fb41846547cd37
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2021-10-23 00:04:35 -0300

    colocando um arquivo de teste h2

--- src/main/java/com/besart/loja/entities/User.java
+++ src/main/java/com/besart/loja/entities/User.java
@@ -2,10 +2,19 @@ package com.besart.loja.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class User  implements Serializable{
 	//pra transformar em bit ou gravar na rede
 	private static final long serialVersionUID = 1L; //usando o processo padrão
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String name;
 	private String email;
