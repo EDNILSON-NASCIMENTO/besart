96d1f091e49562f74159e84bb07c92c7c7efbe7b
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2021-10-22 23:26:49 -0300

    Primeiro commit API BESTART - projeto exemplo

---
+++ src/main/java/com/besart/loja/BesartApplication.java
@@ -0,0 +1,13 @@
+package com.besart.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesartApplication.class, args);
+	}
+
+}
